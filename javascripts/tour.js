d0a3df09605bd73747dbbed5cf4085409e33a0d3
Peregrinator <bob.burbach@gmail.com>
2014-03-06 13:50:28 -0800

    Testing possible solutions to tour.js problems

--- javascripts/tour.js
+++ javascripts/tour.js
@@ -1,16 +1,33 @@
+$(document).ready(function() {
 var tour = new Tour({
   steps: [
   {
-    element: "#money",
+    element: ".fr-head-meta-bar-official",
     title: "fr link stats",
     content: "fr link stats"
   },
   {
-    element: "#environment",
+    element: ".nav-home-carousel",
     title: "icon fr2 globe",
     content: "icon fr2 globe"
   }
 ]});
 
+/*
+var tour = new Tour({
+  steps: [
+  {
+    element: "#h1",
+    title: "fr link stats",
+    content: "fr link stats"
+  },
+  {
+    element: "#h2",
+    title: "icon fr2 globe",
+    content: "icon fr2 globe"
+  }
+]});
+*/
 tour.init();
 tour.start();
+});
